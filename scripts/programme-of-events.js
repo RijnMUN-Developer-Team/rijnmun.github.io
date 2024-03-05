bab5ef340cf4f588079ac23641b8498cebd3dda9
agilfor <alan.gil.forza@gmail.com>
2024-03-05 23:30:38 +0100

    poe transition improvements (I still think it looked better before)

--- scripts/programme-of-events.js
+++ scripts/programme-of-events.js
@@ -1,67 +1,32 @@
 function openKey() {
     let keyTitle = document.getElementsByClassName("key-heading")[0];
+    // let keyH2 = document.getElementById('key-h2')
     let cont = document.getElementsByClassName("key-cont")[0];
     
     if (cont.style.display == "none") {
         // open
-        cont.style.display = "block";
-        cont.style.width = cont.scrollWidth + "px";
+        // keyH2.style = "position: absolute; right: -80px;"
+        // keyH2.style.transition = "unset"
+        // setTimeout(function() {
+            cont.style.display = "block";
+            // cont.style.width = cont.scrollWidth + "px";
+            // keyH2.style.transition = "unset"
+            // keyH2.style = "writing-mode: revert !important; text-orientation: revert !important;"
+            // keyH2.style.transition = "all 0.4s linear"
+            setTimeout(function(){
+                cont.style = "position: absolute; right: 100px;"
+                // keyH2.style = "position: absolute; right: 0px;"
+            }, 10)
+        // }, 200)
 
     } else {
         //close
-        cont.style.width = 0;
+        // cont.style.width = 0;
+        cont.style = "position: absolute; right: -10px;"
+        // keyH2.style = "writing-mode: vertical-rl !important; text-orientation: upright !important;"
 
         setTimeout(function(){
             cont.style.display = 'none'; 
-        }, 200, cont)
-    }
-}
-
-function panel() {
-    let cont = document.getElementsByClassName("key-cont")[0];
-    let bar1 = document.getElementById('bar1')
-    let bar2 = document.getElementById('bar2')
-    let bar3 = document.getElementById('bar3')
-    // let caret = document.getElementById('side-button-i')
-    // caret.style.transition = "0.2s ease-in-out"
-
-    if (cont.style.getPropertyValue("--i") === '0') {
-        // open the contents
-        cont.style.display = 'block';
-        cont.style.height = cont.scrollHeight + "px";
-        cont.style.width = cont.scrollWidth + "px"
-        cont.style.setProperty('--i', '1')
-        // caret.style.transform = "rotate(-90deg)"
-        // document.getElementById("side-button").innerHTML = "<";
-        bar1.style.transition = "0.5s ease-in-out"
-        bar2.style.transition = "0.5s ease-in-out"
-        bar3.style.transition = "0.5s ease-in-out"
-        bar1.style.transform = "rotate(315deg)"
-        bar2.style.transform = "rotate(315deg)"
-        bar3.style.transform = "rotate(225deg)"
-        bar1.style.setProperty('--i', '13')
-        bar2.style.setProperty('--i', '9')
-        bar3.style.setProperty('--i', '5')
-    } else {
-        // close the contents
-        cont.style.height = 0;
-        cont.style.width = 0
-        cont.style.setProperty('--i', '0')
-        // pausing code for animation
-        setTimeout(function(){
-            cont.style.display = 'none'; 
-        }, 200, cont)
-        // caret.style.transform = "rotate(0deg)"
-
-        // document.getElementById("side-button").innerHTML = ">";
-        bar1.style.transition = "0.5s ease-in-out"
-        bar2.style.transition = "0.5s ease-in-out"
-        bar3.style.transition = "0.5s ease-in-out"
-        bar1.style.transform = "rotate(0deg)"
-        bar2.style.transform = "rotate(0deg)"
-        bar3.style.transform = "rotate(0deg)"
-        bar1.style.setProperty('--i', '0')
-        bar2.style.setProperty('--i', '9')
-        bar3.style.setProperty('--i', '18')
+        }, 400)
     }
 }
