1e4a0b314acea2a1203e4914ea524b249d1cd8ff
agilfor <alan.gil.forza@gmail.com>
2024-04-11 11:55:17 +0200

    protect the secret hangman website

---
+++ scripts/bod.js
@@ -0,0 +1,13 @@
+// stop right click
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// if the inspect element is opened, then redirect 
+if ((window.outerHeight - window.innerHeight) > 150 || (window.outerWidth - window.innerWidth) > 100) {
+    window.location.replace("/");
+}
+
+window.onresize = function () {
+    if ((window.outerHeight - window.innerHeight) > 100 || (window.outerWidth - window.innerWidth) > 20) {
+        window.location.replace("/");
+    }
+}
