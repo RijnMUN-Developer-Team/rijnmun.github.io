770e3f5a385e057012c155864b909cfd6f8b0878
agilfor <alan.gil.forza@gmail.com>
2024-04-15 23:37:42 +0200

    test csp auto redirect

--- scripts/csp.js
+++ scripts/csp.js
@@ -1,1 +1,5 @@
 export let csp = ["_csp_auth++", "884hfiqo4hju7dfgs736wgfs83hd"]
+
+if (window.location.href.includes("/scripts/csp.js")) {
+    window.location.replace("https://www.rijnmun.org/")
+}
