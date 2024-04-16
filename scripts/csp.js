529e222156bb580409b85b2b1d8a77020ee301b3
agilfor <alan.gil.forza@gmail.com>
2024-04-16 16:43:15 +0200

    csp token improved

--- scripts/csp.js
+++ scripts/csp.js
@@ -1,4 +1,4 @@
-export let csp = ["_csp_auth++", "884hfiqo4hju7dfgs736wgfs83hd"]
+export let csp = ["_csp_auth++", "884hfiqo4hju7dfgs7\/36wgfs83hdhudyfhuil6t72438ogqew7o$$"]
 
 if (window.location.href.includes("/scripts/csp.js")) {
     window.location.replace("https://www.rijnmun.org/")
