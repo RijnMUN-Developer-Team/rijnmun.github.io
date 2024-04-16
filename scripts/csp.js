1b0e643075e833caa3e2d318bbf193814023c3a3
agilfor <alan.gil.forza@gmail.com>
2024-04-16 21:22:44 +0200

    make csp auth longer

--- scripts/csp.js
+++ scripts/csp.js
@@ -1,4 +1,4 @@
-export let csp = ["_csp_auth++", "884hfiqo4hju7dfgs7"]
+export let csp = ["_csp_auth++", "884hfiqo4hju7dfgs7hruwaepdigylfhi4756890pewhdhrgwuewaipgrquplt373y7q2ephdsglfhuds"]
 
 if (window.location.href.includes("/scripts/csp.js")) {
     window.location.replace("https://www.rijnmun.org/")
