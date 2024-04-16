063efadad55ff554f2b57bd2767b5b2c7236ea85
agilfor <alan.gil.forza@gmail.com>
2024-04-16 21:21:41 +0200

    edit csp code

--- scripts/csp.js
+++ scripts/csp.js
@@ -1,4 +1,4 @@
-export let csp = ["_csp_auth++", "884hfiqo4hju7dfgs7\/36wgfs83hdhudyfhuil6t72438ogqew7o$$"]
+export let csp = ["_csp_auth++", "884hfiqo4hju7dfgs7"]
 
 if (window.location.href.includes("/scripts/csp.js")) {
     window.location.replace("https://www.rijnmun.org/")
