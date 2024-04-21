e06d63f9b35902567ab63f188f346e146fa6a3c4
agilfor <alan.gil.forza@gmail.com>
2024-04-21 23:34:49 +0200

    csp token updated

--- scripts/csp.js
+++ scripts/csp.js
@@ -1,5 +1,1 @@
-export let csp = ["_csp_auth++", "884hfiqo4hju7dfgs7hruwaepdigylfhi4756890pewhdhrgwuewaipgrquplt373y7q2ephdsglfhuds"]
-
-if (window.location.href.includes("/scripts/csp.js")) {
-    window.location.replace("https://www.rijnmun.org/")
-}
+export let csp = ["_csp_auth++", window.btoa("884hfiqo4hju7dfgs7hruwaepdigylfhi4756890pewhdhrgwuewaipgrquplt373y7q2ephdsglfhuds")]
