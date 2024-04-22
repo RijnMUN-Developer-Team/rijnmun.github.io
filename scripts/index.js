b6a104e5d586c3c4bb47491db232d8ca99c4cfbb
Lenka <lenkahuang07@gmail.com>
2024-04-22 22:24:07 +0200

    automatic updating date

---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+// updating the last-updated sidebar date automatically
+// let lastUpdated = new Date(document.lastModified);
+// document.getElementById("date").innerHTML = `Last Updated: ${String(lastUpdated.getDate()).padStart(2, '0')}/${String(lastUpdated.getMonth()+1).padStart(2, '0')}/${lastUpdated.getFullYear()}`;
