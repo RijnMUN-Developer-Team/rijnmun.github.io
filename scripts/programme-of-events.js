1cfa490509d9cef9cf3cf2a39e73c836dd1083f1
Alan Gil Forza <alan.gil.forza@gmail.com>
2024-09-12 21:50:31 +0200

    Finally fixed poe key

--- scripts/programme-of-events.js
+++ scripts/programme-of-events.js
@@ -1,17 +1,60 @@
-function toggle_col_key() {
-    let eye = document.getElementById('eye')
-    let key = document.getElementById('key')
-    if (eye.classList.contains('fa-eye')) {
-        eye.classList.remove('fa-eye')
-        eye.classList.add('fa-eye-slash')
-        key.style.transition = "opacity 0.2s linear"
-        key.style.opacity = 0
-        eye.style.opacity = 1
+// function  toggle_col_key() {
+//     const collapsibles = document.getElementById('collapsible-cols')
+//     const caret = document.getElementById('arrow');
+//     const cont = document.getElementById('sunday-item');
+//     caret.style.transition = "";
+//     collapsibles.style.transition = "";
+//     if (caret.classList.contains('0')) {
+//         caret.style.transform = "rotate(-90deg)";
+//         collapsibles.style.height = '';
+//         caret.classList.remove('0');
+//         caret.classList.add('1');
+//         // setTimeout(() => {
+//         collapsibles.style.display = 'block';
+//         cont.style.height = `${cont.clientHeight - collapsibles.clientHeight}px`
+//         cont.style.maxHeight = `${cont.clientHeight - collapsibles.clientHeight}px`
+//         // }, 200)
+//     } else {
+//         caret.style.transform = "rotate(0deg)";
+//         collapsibles.style.height = '0px';
+//         caret.classList.remove('1');
+//         caret.classList.add('0');
+//         // setTimeout(() => {
+//         collapsibles.style.display = 'none';
+//         cont.style.height = ''
+//         // }, 200)
+//     }
+// }
+
+function  toggle_col_key() {
+    const collapsibles = document.getElementById('collapsible-cols')
+    const caret = document.getElementById('arrow');
+    caret.style.transition = "";
+    collapsibles.style.transition = "";
+    if (caret.classList.contains('0')) {
+        caret.style.transform = "rotate(-90deg)";
+        collapsibles.style.height = '';
+        caret.classList.remove('0');
+        caret.classList.add('1');
+        // setTimeout(() => {
+        collapsibles.style.display = 'block';
+        // }, 200)
     } else {
-        eye.classList.add('fa-eye')
-        eye.classList.remove('fa-eye-slash')
-        key.style.transition = "opacity 0.2s linear"
-        eye.style.opacity = 1
-        key.style.opacity = 1
+        caret.style.transform = "rotate(0deg)";
+        collapsibles.style.height = '0px';
+        caret.classList.remove('1');
+        caret.classList.add('0');
+        // setTimeout(() => {
+        collapsibles.style.display = 'none';
+        // }, 200)
     }
 }
+
+document.addEventListener('scroll', (e) => {
+    const footer = Math.floor(document.querySelector('footer').getBoundingClientRect().height)
+    const height = Math.floor(document.querySelector('html').getBoundingClientRect().height - window.innerHeight)
+    const key = document.getElementById('key-parent')
+    if (window.scrollY > height - footer) {
+        key.style.bottom = `${10 + window.scrollY - (height - footer)}px`
+    }
+})
