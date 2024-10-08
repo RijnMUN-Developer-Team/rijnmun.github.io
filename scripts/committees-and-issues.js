81f9265bef84c332987f5a41711a415330118bb2
Alan Gil Forza <alan.gil.forza@gmail.com>
2024-10-08 22:59:56 +0200

    added resolution updating to all committees (ready to deploy)

--- scripts/committees-and-issues.js
+++ scripts/committees-and-issues.js
@@ -11,21 +11,21 @@ document.addEventListener("DOMContentLoaded", () => {
         active_cont.innerHTML = '<h4>Resolution in Debate</h4>'
         passed_cont.innerHTML = '<h4>Passed Resolutions</h4>'
         if (resos[`${committee}`].approved.length == 0) {
-            approved_cont.innerHTML += `<p class="no-resos">No resos found</p>`
+            approved_cont.innerHTML += `<p class="no-resos">None found</p>`
         } else {
             for (const approved_reso of resos[`${committee}`].approved) {
                 approved_cont.innerHTML += `<p class="reso-link"><a href="${approved_reso.link}">${approved_reso.name}</a></p>`
             }
         }
         if (resos[`${committee}`].active.length == 0) {
-            active_cont.innerHTML += `<p class="no-resos">No resos found</p>`
+            active_cont.innerHTML += `<p class="no-resos">None found</p>`
         } else {
             for (const active_reso of resos[`${committee}`].active) {
                 active_cont.innerHTML += `<p class="reso-link"><a href="${active_reso.link}">${active_reso.name}</a></p>`
             }
         }
         if (resos[`${committee}`].passed.length == 0) {
-            passed_cont.innerHTML += `<p class="no-resos">No resos found</p>`
+            passed_cont.innerHTML += `<p class="no-resos">None found</p>`
         } else {
             for (const passed_reso of resos[`${committee}`].passed) {
                 passed_cont.innerHTML += `<p class="reso-link"><a href="${passed_reso.link}">${passed_reso.name}</a></p>`
