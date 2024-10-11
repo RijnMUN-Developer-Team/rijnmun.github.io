47c6624bdb46a70c650b9e9df1301e0afed7701d
Lenka <lenkahuang07@gmail.com>
2024-10-11 22:40:23 +0200

    photos page

---
+++ scripts/photos.js
@@ -0,0 +1,11 @@
+// number of photos; index 0 is day 1, index 1 is day 2...etc
+const num_photos = [17]
+
+for (let day=1; day<=num_photos.length; day++) {
+    const day_photos_div = document.getElementById(`conference-day${day}`);
+    day_photos_div.innerHTML = "";
+
+    for (let i=1; i<=num_photos[day-1]; i++) {
+        day_photos_div.innerHTML += `<img class="conference-photos" src="/images/conference-photos/day${day}/img1${("0"+i).slice(-2)}.JPG" alt="Day ${day} Image ${i}">`;
+    }
+}
