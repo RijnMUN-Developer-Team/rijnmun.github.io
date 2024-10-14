0325c1142b09aa267e14c5562ef96bbae9aad2d5
Lenka <lenkahuang07@gmail.com>
2024-10-14 14:59:20 +0200

    lazyload enabled

--- scripts/photos.js
+++ scripts/photos.js
@@ -6,6 +6,8 @@ for (let day=1; day<=num_photos.length; day++) {
     day_photos_div.innerHTML = "";
 
     for (let i=1; i<=num_photos[day-1]; i++) {
-        day_photos_div.innerHTML += `<img loading="lazy" class="conference-photos" src="/images/conference-photos/day${day}/img${day}${("0"+i).slice(-2)}.JPG" alt="Day ${day} Image ${i}">`;
+        // day_photos_div.innerHTML += `<img loading="lazy" class="conference-photos" src="/images/conference-photos/day${day}/img${day}${("0"+i).slice(-2)}.JPG" alt="Day ${day} Image ${i}">`;
+        day_photos_div.innerHTML += `<img class="conference-photos" src="/images/conference-photos/day${day}/img${day}${("0"+i).slice(-2)}.JPG" alt="Day ${day} Image ${i}">`;
+
     }
 }
